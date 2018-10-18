70daaa7c0f357d2f52981448bb66f9a0cedb19bb
Nicolas Goudry <goudry.nicolas@gmail.com>
2018-10-18 12:09:57 +0200

    Load SQL files in seed folder and execute them

--- src/server/boot/db/seed.js
+++ src/server/boot/db/seed.js
@@ -6,14 +6,14 @@ import { loadFile } from 'sequelize-fixtures'
 const debug = createDebugger('hapi-starter:seed')
 
 /**
- * This function loads json fixtures files in src/database/seed folder
+ * This function loads JSON fixtures files/SQL files in seed folder
  * and automatically generates a SQL query to set Sequelize timefields
  * default values to NOW() SQL function
  * @param {Object} db Sequelize database instance
  * @returns {Promise}
  */
 const seedDatabase = async (db) => {
-  debug('Generate query')
+  debug('Generate query for timefields default value')
 
   // We iterate over model names to generate query
   const modelNames = Object.keys(db.models)
@@ -28,17 +28,21 @@ ALTER updated_at SET DEFAULT NOW();
   }
 
   debug('Query generated')
-  debug(defaultNow)
-  debug('Check for fixtures file')
 
   const seedPath = path.resolve(__dirname, '../../../database/seed')
-  const hasJsonFiles = fs.readdirSync(seedPath).reduce((acc, val) => {
-    if (!acc) {
-      acc = /.*\.json$/.test(val)
-    }
+  const seedFiles = fs.readdirSync(seedPath)
+
+  debug('Search for seed files (SQL and JSON)')
+
+  // Search and load SQL files into seed directory
+  const sqlQueries = seedFiles.filter((file) => /.*\.sql$/.test(file)).reduce((acc, file) => {
+    acc.push(fs.readFileSync(path.resolve(seedPath, file)))
 
     return acc
-  }, false)
+  }, []).join('\n')
+
+  // Check into seed directory for JSON files
+  const hasJsonFiles = seedFiles.filter((file) => /.*\.json$/.test(file)).length > 0
 
   if (hasJsonFiles) {
     debug('Fixtures file found')
@@ -49,6 +53,7 @@ ALTER updated_at SET DEFAULT NOW();
   // Execute query and load fixtures files
   return Promise.all([
     db.sequelize.query(defaultNow),
+    db.sequelize.query(sqlQueries),
     hasJsonFiles && loadFile(path.resolve(seedPath, '*.json'), db.models)
   ])
 }
